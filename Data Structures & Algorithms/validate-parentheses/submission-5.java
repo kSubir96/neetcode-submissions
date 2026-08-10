bb44e1266454e21873b6087ffbd5f8fcb66cdfd5
kSubir96 <79801961+kSubir96@users.noreply.github.com>
2026-08-11 03:15:20 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        int i = 0, n = s.length();
+        if(n%2 != 0) return false;
+        Map<Character,Character> map = Map.of(')', '(', '}', '{', ']', '[');
+
+        while(i<n) {
+            char t = s.charAt(i);
+            if(map.values().contains(t))
+                st.push(t);
+            else {
+                if(st.size() == 0) return false;
+                if(st.peek() == map.get(t)) {
+                    System.out.println("matched");
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+            System.out.println("s: "+ st);
+            i++;
+        }
+        return st.size() == 0;
+    }
+}
